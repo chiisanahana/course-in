b3cf24ee647dd1285eefc5e9923329b9ad221e6e
vincentlee23 <vincentlee.vk23@gmail.com>
2022-07-23 13:18:58 +0700

    seeding user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -22,5 +22,41 @@ class UserSeeder extends Seeder
             'password' => bcrypt('password'),
             'role_id' => 1,
         ]);
+
+        User::create([
+            'name' => 'Calista Daphne',
+            'email' => 'calista@gmail.com',
+            'wa_number' => '0812-7630-3591',
+            'address' => 'Medan',
+            'password' => bcrypt('password'),
+            'role_id' => 2,
+        ]);
+
+        User::create([
+            'name' => 'Ivanco Gratio Hartono',
+            'email' => 'ivanco@gmail.com',
+            'wa_number' => '0823-7199-4324',
+            'address' => 'Palembang',
+            'password' => bcrypt('password'),
+            'role_id' => 2,
+        ]);
+
+        User::create([
+            'name' => 'Mulyani',
+            'email' => 'mulyani@gmail.com',
+            'wa_number' => '0822-4690-9839',
+            'address' => 'Bogor',
+            'password' => bcrypt('password'),
+            'role_id' => 2,
+        ]);
+
+        User::create([
+            'name' => 'Vincent',
+            'email' => 'vincent@gmail.com',
+            'wa_number' => '0895-6059-85036',
+            'address' => 'Jakarta',
+            'password' => bcrypt('password'),
+            'role_id' => 2,
+        ]);
     }
 }
