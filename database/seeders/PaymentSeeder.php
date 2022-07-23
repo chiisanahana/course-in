50ee748b68766a1c4bfc83f1b466c627b3c14ed8
vincentlee23 <vincentlee.vk23@gmail.com>
2022-07-23 13:26:32 +0700

    ga nambah apa2

--- database/seeders/PaymentSeeder.php
+++ database/seeders/PaymentSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Payment;
 use Illuminate\Database\Seeder;
 
 class PaymentSeeder extends Seeder
@@ -13,6 +14,22 @@ class PaymentSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // Payment::create([
+        //     'lesson_id' => '1',
+        //     'user_id' => '2',
+        //     'promo_id' => '0812-1234-4321',
+        //     'address' => 'Jakarta',
+        //     'password' => bcrypt('password'),
+        //     'role_id' => 1,
+        // ]);
+
+        // $table->id(); 
+        //     $table->foreignId('lesson_id')->constrained();
+        //     $table->foreignId('user_id')->constrained();
+        //     $table->integer('promo_id')->default(0);
+        //     $table->string('payment_method');
+        //     $table->integer('amount');
+        //     $table->integer('ratings')->default(0);
+        //     $table->timestamps();
     }
 }
